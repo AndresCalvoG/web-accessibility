9c6858bbebdb91dac3c14fd09d6556be5918547e
AndresCalvoG <andrescalvo9407@gmail.com>
2024-05-09 20:15:09 -0500

    last changes

---
+++ index2.js
@@ -0,0 +1,15 @@
+/*
+manejar el foco del teclado: 
+tabindex: 
+ *con valor (-1) eleelemento debe ser enfocado pero no accesible a 
+  traves del teclado.
+ *con valor (0>) positivo debe ser enfocado y su orden relativo es definido por el
+  valor del atributo.
+ *con valor de (0) debe ser enfocado y ser accesible a traves de 
+  la navegacion secuencial del teclado, pero su orden relativo
+  es definido por convension de la plataforma.
+
+  with 'keyup' event evalua el valor de keycode y realiza acciones
+
+  area-live="" // puede recibir dos valores assertive o polite
+*/
